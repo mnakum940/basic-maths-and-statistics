613d656fb6f4573c1288ee842b4e8bce939b623a
Meet Nakum <65437506+mnakum940@users.noreply.github.com>
2021-01-08 21:30:28 +0530

    Add files via upload

---
+++ maths_and_statistics.py
@@ -0,0 +1,12 @@
+number1=[12,19,15,22,17]
+
+mean = sum(number1)/len(number1)
+print("mean",mean)
+
+#odd number
+number_odd=[12,15,17,19,22]
+
+collection_of_no= sorted(number_odd)
+number = (len(collection_of_no)//2+1)
+Index =  collection_of_no[number-1]
+print("Median_Odd",Index)
